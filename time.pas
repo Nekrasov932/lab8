ca7d5c85e8ec954d9ad160d066cd9b30e93fdfef
Некрасов Алексей <nekrasovad2005@gmail.com>
2023-09-30 11:14:22 +0300

    Initial commit

--- time.pas
+++ time.pas
@@ -2,5 +2,5 @@
 var a := ReadInteger('Введите год:');
 if a mod 4 = 0 then
   if (a mod 100 = 0) and (a mod 400 <> 0) then 
-    print ('Не высокосный')
-  else print('Высокосный')
+    print ('Не высокосный, в году 365 дней')
+  else print('Высокосный, в году 366 дней')
